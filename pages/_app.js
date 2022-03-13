8582e895d62c4c37ae2bc3d432358d2743b0230b
zinia <zmcrtrinidad@neu.edu.ph>
2022-03-13 22:50:29 +0800

    remove scripts

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
 import '../styles/globals.css'
 import Layouts from '../components/Layouts'
-// _app.js
 
 import '@fortawesome/fontawesome-free/css/all.css';
+
 
 // ...
 
@@ -10,7 +10,6 @@ function MyApp({ Component, pageProps }) {
   return (
     <Layouts >
       <Component {...pageProps} />
-      <script src="https://unpkg.com/tippy.js@4"></script>
     </Layouts>
   )
 }
